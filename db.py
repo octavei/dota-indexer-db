31bf6a8c0f7d93b5473774c56df11e011f649706
octavei <octavei@proton.me>
2024-01-29 15:56:25 +0800

    添加mint deploy currency表

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table, text, CheckConstraint, BigInteger, DECIMAL, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table, text, CheckConstraint, BigInteger, \
+    DECIMAL, DateTime
 from sqlalchemy.orm import declarative_base, Session
 from sqlalchemy.dialects.mysql import insert
 
@@ -10,31 +11,81 @@ class DotaDB:
         self.session = Session(bind=self.engine)
         # self.metadata.create_all(bind=self.engine)
 
-    def __table(self, table_name: str):
-        return Table(table_name, self.metadata,
-                     Column('id', Integer, primary_key=True),
-                     Column('name', String(255)),
+    # 直接一个资产表 程序启动直接创建
+    def _currency_table(self, tick: str):
+        return Table(tick + "_currency", self.metadata,
+                     Column('user', String(64), nullable=False, primary_key=True),
+                     Column('tick', String(8), nullable=False, server_default=tick, primary_key=True),
+                     Column('balance', DECIMAL(46, 18), default=0, nullable=False),
                      )
 
-    def create_table(self, table_name):
-        # 定义表格
-        table = self.__table(table_name)
-        # 创建表格
+    # 部署表  程序启动直接创建
+    def _deploy_table(self):
+        return Table("deploy", self.metadata,
+                     Column('id', Integer, autoincrement=True),
+                     Column("deployer", String(64), nullable=False),
+
+                     # 用于标记这个部署事件在链上哪个区块高度哪个位置
+                     Column("block_height", Integer, nullable=False),
+                     Column("block_hash", String(64), nullable=False),
+                     Column("extrinsic_index", Integer, nullable=False),
+                     Column("batchall_index", Integer, default=0),
+                     Column("remark_index", Integer, default=0),
+
+                     Column("p", String(8), primary_key=True, server_default="dot-20", nullable=False),
+                     Column('op', String(16), server_default="deploy", nullable=False),
+                     Column("tick", String(8), primary_key=True, nullable=False),
+                     Column('decimal', Integer, default=18),
+                     Column("mode", String(8), default="fair", nullable=False),
+                     Column("amt", DECIMAL(46, 18), default=0),
+                     Column("start", Integer, default=0),
+                     Column("end", Integer, default=0),
+                     Column("max", DECIMAL(46, 18), default=0),
+                     Column("lim", DECIMAL(46, 18), default=0),
+                     Column("admin", String(64)),
+                     )
+
+    # mint table
+    def _mint_table(self, tick: str):
+        return Table(tick + "_mint", self.metadata,
+                     Column('id', Integer, autoincrement=True),
+
+                     Column("singer", String(64), nullable=False, primary_key=True),
+                     Column("block_height", Integer, nullable=False, primary_key=True),
+                     Column("block_hash", String(64), nullable=False),
+                     Column("extrinsic_index", Integer, nullable=False, primary_key=True),
+                     Column("batchall_index", Integer, default=0, primary_key=True),
+                     Column("remark_index", Integer, default=0, primary_key=True),
+
+                     Column("p", String(8), server_default="dot-20", nullable=False),
+                     Column('op', String(16), server_default="mint", nullable=False),
+                     Column("tick", String(8), server_default=tick, nullable=False),
+                     Column("to", String(64), nullable=False),
+                     Column("lim", DECIMAL(46, 18), default=0),
+                     )
+
+    def create_tables_for_new_tick(self, tick: str):
+        # 创建currency表
+        currency_table = self._currency_table(tick)
+        # 创建mint表
+        mint_table = self._mint_table(tick)
         self.metadata.create_all(bind=self.engine)
 
-    # def insert_or_update(self, ):
-    #     with self.session.begin():
-    #         stmt = insert(table).values(id=id, name=name)
-    #         stmt = stmt.on_duplicate_key_update(name=stmt.inserted.name)
-    #         self.session.execute(stmt)
+    def insert_or_update(self, ):
+        with self.session.begin():
+            pass
+            # stmt = insert(table).values(id=id, name=name)
+            # stmt = stmt.on_duplicate_key_update(name=stmt.inserted.name)
+            # self.session.execute(stmt)
 
     # def select(self):
     #     se = self.table.select()
     #     result = self.session.execute(se).fetchall()
     #     print("r:", result)
     #
-    # def delete(self, table_name: str):
-    #     self.session.execute(self.table.delete())
+
+    def delete(self, table_name: str):
+        self.session.execute(self.table.delete())
 
     def close(self):
         self.session.close()
@@ -43,4 +94,4 @@ class DotaDB:
 if __name__ == "__main__":
     url = 'mysql+mysqlconnector://root:116000@localhost/wjy'
     db = DotaDB(url)
-    db.create_table("haha")
+    db.create_tables_for_new_tick("dota")
