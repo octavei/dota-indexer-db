a67ecdda41fde4e5dbd789fc39e3fcf8badeff3b
octavei <octavei@proton.me>
2024-01-31 00:38:27 +0800

    更新

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dotadb',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     install_requires=[
         'websocket-client>=0.57.0,<2',
